11b164db621b3f363d863ee0cd3a479eb53c9e8a
dharshan-m <dharshanmohan2704@gmail.com.com>
2026-05-08 15:36:48 +0530

    removed unwanted forms(audio, food, transport and accommodation)

--- client/src/Components/Forms/AudioForm.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function AudioForm() {
-  return (
-    <div>AudioForm</div>
-  )
-}
--- client/src/Components/Forms/TransportForm.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function TransportForm() {
-  return (
-    <div>TransportForm</div>
-  )
-}
