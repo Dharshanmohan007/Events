5afb355898287256f4d0869a897b4c6b71427d35
dharshan-m <dharshanmohan2704@gmail.com>
2026-08-01 17:28:55 +0530

    developed a new page for other management tab for inventry - accommodation

--- client/src/Pages/Dashboards/Admin-Dashboard/AdminDashboardHeader.jsx
+++ client/src/Pages/Dashboards/Admin-Dashboard/AdminDashboardHeader.jsx
@@ -11,12 +11,13 @@ const adminRoutes = {
     faculty: '/dashboard-admin/FacultyManagement',
     admins: '/dashboard-admin/AdminManagement',
     reports: '/dashboard-admin/reports',
+    othermanagements: '/dashboard-admin/other-managements',
 }
 
 const getNavLinkClassName = ({ isActive }) =>
     `font-medium ${isActive ? 'border-b border-[#8B5CF6] text-[#8B5CF6]' : 'text-[#FFFFFF80]'}`
 
-const AdminDashboardHeader = () => {
+const   AdminDashboardHeader = () => {
     return (
         <>
             <section className='dashboard-header bg-[#0a0e18] flex items-center justify-between py-4 px-4 poppins sticky top-0 z-10 '>
@@ -30,6 +31,7 @@ const AdminDashboardHeader = () => {
                         <NavLink to={adminRoutes.faculty} className={getNavLinkClassName}>Faculty Management</NavLink>
                         {/* <NavLink to={adminRoutes.admins} className={getNavLinkClassName}>Admin Management</NavLink> */}
                         <NavLink to={adminRoutes.reports} className={getNavLinkClassName}>Reports</NavLink>
+                        <NavLink to={adminRoutes.othermanagements} className={getNavLinkClassName}>Other Managements</NavLink>
                     </div>
                 </div>
                 <div className="second-container flex items-center gap-6 ">
