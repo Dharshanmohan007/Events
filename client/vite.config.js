4abef2abb5ff5d7c09185eb5dcd82890cf8a1d02
Abishek6702 <abishekkrishnat@gmail.com>
2026-09-01 15:26:29 +0530

    change

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,13 +2,27 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     tailwindcss(),
     react()
   ],
+
   server: {
-    port: 5173,
-  },
+    port: 5127,
+    host: '0.0.0.0',
+    strictPort: false,
+
+    hmr: {
+      clientPort: 5127
+    },
+
+    allowedHosts: [
+      'srieshwarevents.com',
+      'www.srieshwarevents.com',
+      '.trycloudflare.com',
+      'localhost',
+      '127.0.0.1'
+    ]
+  }
 })
